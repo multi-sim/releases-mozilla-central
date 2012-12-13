1c01cc222942a705e58c03ff919ab00bf56a452d
Ken Chang <kchang@mozilla.com>
2012-12-13 14:15:30 +0800

    Bug 814629 - part3 - add mobile connection test scripts for multiSIM

---
+++ dom/network/tests/marionette/test_mobile_iccinfo_multisim_1.js
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 30000;
+
+SpecialPowers.addPermission("mobileconnection", true, document);
+
+let connection = navigator.mozMobileConnectionManager.defaultMobileConnection;
+ok(connection instanceof MozMobileConnection,
+   "connection is instanceof " + connection.constructor);
+
+// The emulator's hard coded iccid value.
+// See it here {B2G_HOME}/external/qemu/telephony/sim_card.c#L299.
+is(connection.iccInfo.iccid, 89014103211118510720);
+
+// The emulator's hard coded mcc and mnc codes.
+// See it here {B2G_HOME}/external/qemu/telephony/android_modem.c#L2465.
+is(connection.iccInfo.mcc, 310);
+is(connection.iccInfo.mnc, 260);
+is(connection.iccInfo.spn, "Android");
+// Phone number is hardcoded in MSISDN
+// See {B2G_HOME}/external/qemu/telephony/sim_card.c, in asimcard_io()
+is(connection.iccInfo.msisdn, "15555215554");
+
+SpecialPowers.removePermission("mobileconnection", document);
+finish();
